9a47c811b1331d5e2d168543d3dcf6f7b316be24
Sharath <sharath.esr@gmail.com>
2023-07-31 15:12:09 +0530

    feat: added explore page along with search feature

--- src/Pages/Explore/explore.jsx
+++ src/Pages/Explore/explore.jsx
@@ -1,8 +1,27 @@
+import { useContext, useState } from 'react';
 import './explore.css';
+import { VideoCard } from '../../Components';
+import { DataContext } from '../../contexts/dataContext';
+
 export function Explore(){
+    const {state} = useContext(DataContext);
+    const [searchInput, setSearchInput] = useState('');
+
+    const displayVideos = searchInput.length >0
+    ? state.videos.filter(({title}) => title.toLowerCase().includes(searchInput.toLowerCase()))
+    : state.videos
+
     return (
         <div>
             <h1>Explore Page</h1>
+            <input type='text' placeholder='search videos by title' className='search-input' onChange={(e)=> setSearchInput(e.target.value)}/>
+            <ul className='video-container'>
+                {
+                    displayVideos?.map(list => <li key={list._id}>
+                        <VideoCard video={list} />
+                    </li>)
+                }
+            </ul>
         </div>
     )
 }
